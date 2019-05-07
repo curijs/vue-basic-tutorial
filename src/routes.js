e4f4da5610273a4867e95b82bcd5b362bbcb4179
Paul Sherman <paul.sherman.88@gmail.com>
2019-05-07 14:54:56 -0500

    Update Curi & Hickory

--- src/routes.js
+++ src/routes.js
@@ -5,43 +5,41 @@ import Book from './components/Book';
 import Checkout from './components/Checkout';
 import NotFound from './components/NotFound';
 
-export default prepareRoutes({
-  routes: [
-    {
-      name: "Home",
-      path: "",
-      respond() {
-        return {
-          body: Home
-        };
-      }
-    },
-    {
-      name: "Book",
-      path: "book/:id",
-      respond() {
-        return {
-          body: Book
-        };
-      }
-    },
-    {
-      name: "Checkout",
-      path: "checkout",
-      respond() {
-        return {
-          body: Checkout
-        };
-      }
-    },
-    {
-      name: "Catch All",
-      path: "(.*)",
-      respond() {
-        return {
-          body: NotFound
-        };
-      }
+export default prepareRoutes([
+  {
+    name: "Home",
+    path: "",
+    respond() {
+      return {
+        body: Home
+      };
     }
-  ]
-});
+  },
+  {
+    name: "Book",
+    path: "book/:id",
+    respond() {
+      return {
+        body: Book
+      };
+    }
+  },
+  {
+    name: "Checkout",
+    path: "checkout",
+    respond() {
+      return {
+        body: Checkout
+      };
+    }
+  },
+  {
+    name: "Catch All",
+    path: "(.*)",
+    respond() {
+      return {
+        body: NotFound
+      };
+    }
+  }
+]);
