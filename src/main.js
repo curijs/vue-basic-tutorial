ae8a51955f86586ce07758591dba478cf1d72530
Paul Sherman <paul.sherman.88@gmail.com>
2019-05-08 14:02:26 -0500

    Announce navigation

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
-import { createRouter } from '@curi/router';
-import { browser } from '@hickory/browser'
-import { CuriPlugin } from '@curi/vue'
+import { createRouter, announce } from '@curi/router';
+import { browser } from '@hickory/browser';
+import { CuriPlugin } from '@curi/vue';
 
 import routes from './routes';
-import App from './App.vue'
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-const router = createRouter(browser, routes)
-Vue.use(CuriPlugin, { router })
+const router = createRouter(browser, routes, {
+  sideEffects: [
+    announce(({ response }) => {
+      return `Navigated to ${response.location.pathname}`;
+    })
+  ]
+});
+Vue.use(CuriPlugin, { router });
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
